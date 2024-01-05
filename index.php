840166bbda60478a20d698e95627886f43c00fd9
clement guillemot <guillemot.clement35@gmail.com>
2024-01-05 19:55:20 +0100

    Ajout des données BDD. Bug lors de la soumission du formulaire

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <!-- inclusion des variables et fonctions -->
-
 <?php
 session_start();
+require_once(__DIR__ . '/config/mysql.php');
+require_once(__DIR__ . '/databaseconnect.php');
 require_once(__DIR__ . '/variables.php');
 require_once(__DIR__ . '/functions.php');
 ?>
@@ -13,9 +14,11 @@ require_once(__DIR__ . '/functions.php');
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Site de recettes - Page d'accueil</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+    >
 </head>
-
 <body class="d-flex flex-column min-vh-100">
     <div class="container">
         <!-- inclusion de l'entête du site -->

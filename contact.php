240f99bffb4870307b187d38a74531b563753ddb
clement guillemot <guillemot.clement35@gmail.com>
2024-01-06 14:16:41 +0100

    blablabla

--- contact.php
+++ contact.php
@@ -1,7 +1,7 @@
+<?php session_start(); ?>
+
 <!DOCTYPE html>
 <html>
-
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,13 +9,12 @@
     <title>Site de Recettes - Page d'accueil</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
-
-
 <body class="d-flex flex-column min-vh-100">
     <div class="container">
+
         <?php require_once(__DIR__ . '/header.php'); ?>
         <h1>Contactez nous</h1>
-        <form action="submit_contact.php" method="post" enctype="multipart/form-data">
+        <form action="submit_contact.php" method="POST" enctype="multipart/form-data">
             <div class="mb-3">
                 <label for="email" class="form-label">Email</label>
                 <input type="email" class="form-control" id="email" name="email" aria-describedby="email-help">
@@ -25,16 +24,14 @@
                 <label for="message" class="form-label">Votre message</label>
                 <textarea class="form-control" placeholder="Exprimez vous" id="message" name="message"></textarea>
             </div>
-            <div class="input-group mb-3">
-                <label for="file" class="input-group-text">Fichier à envoyer :</label>
-                <input type="file" name="file" id="file" class="form-control">
+            <div class="mb-3">
+                <label for="screenshot" class="form-label">Votre capture d'écran</label>
+                <input type="file" class="form-control" id="screenshot" name="screenshot" />
             </div>
             <button type="submit" class="btn btn-primary">Envoyer</button>
         </form>
-        <br />
     </div>
+
     <?php require_once(__DIR__ . '/footer.php'); ?>
 </body>
-
-
 </html>
--- header.php
+++ header.php
@@ -1,4 +1,3 @@
-<!-- header.php -->
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
         <a class="navbar-brand" href="index.php">Site de recettes</a>
@@ -13,9 +12,12 @@
                 <li class="nav-item">
                     <a class="nav-link" href="contact.php">Contact</a>
                 </li>
-                <?php if(isset($_SESSION['LOGGED_USER'])) : ?> 
+                <?php if (isset($_SESSION['LOGGED_USER'])) : ?>
                     <li class="nav-item">
-                        <a href="logout.php" class="nav-link">Deconnexion</a>
+                        <a class="nav-link" href="recipes_create.php">Ajoutez une recette !</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="logout.php">Déconnexion</a>
                     </li>
                 <?php endif; ?>
             </ul>
